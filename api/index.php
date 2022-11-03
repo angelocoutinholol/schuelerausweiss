ee6410e4c786f57443f6a63c4d8848102e041241
Angelo <angelo@wemotion.io>
2022-11-03 09:05:57 +0100

    add backend modules

---
+++ api/index.php
@@ -0,0 +1,31 @@
+<?
+class Controller{
+    private $response;
+    private $username;
+    private $password;
+
+    public function __construct(){
+        // require('WorkingTime_model.php');
+
+        $this->response = ['status' => 'error', 'message' => 'Authentifiziereung fehlgeschlagen'];
+        $this->username = isset($_POST['username']) ? $_POST['username'] : null;
+        $this->password = isset($_POST['password']) ? $_POST['password'] : null;
+    }
+
+    private function setResponse($status = 'error', $message = null){
+        $this->response['status'] = $status;
+        $this->response['message'] = $message;
+    }
+
+    public function execute(){{}
+        if($this->username === 'root' && $this->password == 'password'){
+            $this->setResponse('success', 'Login Erfolgreich!');
+        }
+        
+        print(json_encode($this->response));
+    }
+}
+
+$controller = new Controller();
+$controller->execute();
+?>
